2f6a77e37aa374308584d6fcd51eed5a21c601b2
uttamboda <167513333+uttamboda@users.noreply.github.com>
2025-01-24 16:53:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-const inputs = document.querySelectorAll('.code');
+document.addEventListener("DOMContentLoaded", function () {
+    const inputs = document.querySelectorAll('.code');
+    
+    inputs[0].focus();
 
-inputs.forEach((input, index) => {
-    input.addEventListener('input', (event) => {
-        if (event.target.value.length === 1 && index < inputs.length - 1) {
-            inputs[index + 1].focus();
-        }
-    });
-
-    input.addEventListener('keydown', (event) => {
-        if (event.key === 'Backspace' && event.target.value === '') {
-            if (index > 0) {
-                inputs[index - 1].focus();
+    inputs.forEach((input, index) => {
+        input.addEventListener('input', (event) => {
+            if (event.target.value.length === 1 && index < inputs.length - 1) {
+                inputs[index + 1].focus();
             }
-        }
+        });
+
+        input.addEventListener('keydown', (event) => {
+            if (event.key === 'Backspace' && event.target.value === '') {
+                if (index > 0) {
+                    inputs[index - 1].focus();
+                }
+            }
+        });
     });
 });
